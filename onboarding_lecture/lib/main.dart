6f0accb39c5888cf6261e662917d4e4565d1fd4a
leeseewoo <leeseewoo@gmail.com>
2022-06-01 15:17:11 +0900

    Initial

---
+++ onboarding_lecture/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'onboarding.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: OnBoardingPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Page'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text(
+            'Main Screen',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => OnBoardingPage()));
+              },
+              child: const Text('Go to onboarding screen')),
+        ]),
+      ),
+    );
+  }
+}
